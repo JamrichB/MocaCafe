53b18be0ee988a608705bf3d7a6ed9bbb74d9268
JamrichB <barbor.jamrichova@gmail.com>
2021-05-13 19:47:19 +0200

    AboutUs+video

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let options = {
+    root: null,
+    rootMargin:'0px',
+    threshold:1.0
+};
+let callback = (entries, observer)=>{
+    entries.forEach(entry => {
+        if(entry.target.id == 'cafeVideo')
+        {
+            if (entry.isIntersecting){
+                entry.target.play();
+            }
+            else {
+                entry.target.pause();
+            }
+        }
+    });
+}
+let observer = new IntersectionObserver(callback, options);
+observer.observe(document.querySelector('#cafeVideo'));
