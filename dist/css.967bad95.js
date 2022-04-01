a2dc1d5832de5d1e297f3d6c5dabad8508adf368
Barbora Jamrichová <63923393+JamrichB@users.noreply.github.com>
2022-04-01 16:16:51 +0200

    commit

--- dist/css.967bad95.js
+++ dist/css.967bad95.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\img\\header.png":[["header.848e1042.png","img/header.png"],"img/header.png"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\img\\header.png":[["header.848e1042.png","img/header.png"],"img/header.png"],"./..\\img\\reservation_bg.png":[["reservation_bg.d3f2a7fe.png","img/reservation_bg.png"],"img/reservation_bg.png"],"./..\\img\\booking.png":[["booking.f00b0069.png","img/booking.png"],"img/booking.png"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49638" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52977" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+
+
+
 let options = {
     root: null,
-    rootMargin:'0px',
+    rootMargin:'200px',
     threshold:1.0
 };
 let callback = (entries, observer)=>{
@@ -8,7 +11,7 @@ let callback = (entries, observer)=>{
         if(entry.target.id == 'cafeVideo')
         {
             if (entry.isIntersecting){
-                entry.target.play();
+                entry.target.play(); 
             }
             else {
                 entry.target.pause();
@@ -19,8 +22,170 @@ let callback = (entries, observer)=>{
 let observer = new IntersectionObserver(callback, options);
 observer.observe(document.querySelector('#cafeVideo'));
 
-var moment = require('moment'); // require
 
-var myDate = new Date();
-var updatedDate = moment(myDate).format('dddd');
-console.log(updatedDate);
+
+
+// eslint-disable-next-line no-undef
+$( function() {
+	// eslint-disable-next-line no-undef
+	$( "#datepicker" ).datepicker({
+		dateFormat: "dd-mm-yy"
+		,	duration: "fast"
+	});
+} );
+
+
+      var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        },
+        
+      });
+
+
+let swiper1 = document.querySelector('.swiper1');
+let swiper2 = document.querySelector('.swiper2');
+let swiper3 = document.querySelector('.swiper3');
+let swiper4 = document.querySelector('.swiper4');
+let swiper5 = document.querySelector('.swiper5');
+
+let button1 = document.querySelector('#btn1');
+let button2 = document.querySelector('#btn2');
+let button3 = document.querySelector('#btn3');
+let button4 = document.querySelector('#btn4');
+let button5 = document.querySelector('#btn5');
+
+
+button1.classList.add("active");
+swiper1.style.display = "block";
+swiper2.style.display = "none"; 
+swiper3.style.display = "none";
+swiper4.style.display = "none";
+swiper5.style.display = "none";
+
+
+
+
+button1.addEventListener('click', function() {
+    swiper1.style.display = "block";
+    swiper2.style.display = "none"; 
+    swiper3.style.display = "none";
+    swiper4.style.display = "none";
+    swiper5.style.display = "none";
+
+    button1.classList.add("active");
+    button2.classList.remove("active");
+    button3.classList.remove("active");
+    button4.classList.remove("active");
+    button5.classList.remove("active");
+
+})
+
+
+button2.addEventListener('click', function() {
+    swiper1.style.display = "none";
+    swiper2.style.display = "block"; 
+    swiper3.style.display = "none";
+    swiper4.style.display = "none";
+    swiper5.style.display = "none";
+
+    
+    button1.classList.remove("active");
+    button2.classList.add("active");
+    button3.classList.remove("active");
+    button4.classList.remove("active");
+    button5.classList.remove("active");
+})
+
+button3.addEventListener('click', function() {
+    swiper1.style.display = "none";
+    swiper2.style.display = "none"; 
+    swiper3.style.display = "block";
+    swiper4.style.display = "none";
+    swiper5.style.display = "none";
+
+    
+    button1.classList.remove("active");
+    button2.classList.remove("active");
+    button3.classList.add("active");
+    button4.classList.remove("active");
+    button5.classList.remove("active");
+})
+
+button4.addEventListener('click', function() {
+    swiper1.style.display = "none";
+    swiper2.style.display = "none"; 
+    swiper3.style.display = "none";
+    swiper4.style.display = "block";
+    swiper5.style.display = "none";
+
+
+    button1.classList.remove("active");
+    button2.classList.remove("active");
+    button3.classList.remove("active");
+    button4.classList.add("active");
+    button5.classList.remove("active");
+
+})
+
+button5.addEventListener('click', function() {
+    swiper1.style.display = "none";
+    swiper2.style.display = "none"; 
+    swiper3.style.display = "none";
+    swiper4.style.display = "none";
+    swiper5.style.display = "block";
+
+    button1.classList.remove("active");
+    button2.classList.remove("active");
+    button3.classList.remove("active");
+    button4.classList.remove("active");
+    button5.classList.add("active");
+})
+
+
+// eslint-disable-next-line no-undef
+lightGallery(document.getElementById('animated-thumbnails'), {
+    // Add a custom class to apply style only for the particular gallery
+    addClass: 'lg-custom-thumbnails',
+
+    // Remove the starting animations.
+    // This can be done by overriding CSS as well
+    animateThumb: false,
+    allowMediaOverlap: true,
+});
+
+
+let arrow = document.querySelector('.fixed-arrow');
+let value = 200;
+window.addEventListener('scroll', function() {
+	var position = this.window.scrollY;
+
+    if (position >= value) {
+        arrow.style.display = "block";
+    } else {
+        arrow.style.display = "none";
+        }
+
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
